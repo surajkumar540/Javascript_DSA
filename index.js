e2a17521d15f013df53dab29eea2954b7eb79026
suraj <surajgautam56878@gmail.com>
2024-10-08 13:33:47 +0530

    reverse string by chracter and word

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("jessssssselo")
+// Reverse String
+// let str = "hello world" -> dlrow olleh
+                        // -> world hello
+let str = "hello world"
+function reverseStr(str){
+    // return str.split("").reverse().join(""); // dlrow olleh
+    return  str.split(" ").reverse().join(" ");  // world hello
+}
+
+console.log(reverseStr(str))
